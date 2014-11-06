73d9f631150dddea6ccc90dc02214967d594787c
BrandonLMorris <bmorris95@me.com>
2014-11-06 14:10:16 -0600

    Fixed diagonal movement logic

--- Super Mario Checkers/Checker.java
+++ Super Mario Checkers/Checker.java
@@ -8,36 +8,56 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Checker extends Actor
 {
-    /**
-     * Act - do whatever the GreenChecker wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
 
     //Class variable used to denote which team the checker is on
     public int team;
+    private int startX;
+    private int startY;
 
     public Checker(int team){
         this.team = team;
+        if(team == 1) {
+            setImage(new GreenfootImage("button-red.png"));
+        } else if (team == 2) {
+            setImage(new GreenfootImage("button-green.png"));
+        }
     }
 
     public void act() 
     {   
+        if(Greenfoot.mousePressed(this)) {
+            startX = getX();
+            startY = getY();
+        }
         //Enables the checker to respond to mouse drags
         //Only moves the checker to diagonal posisitons
         if(Greenfoot.mouseDragged(this)) {
             MouseInfo mouse = Greenfoot.getMouseInfo();
-            if((mouse.getX() + mouse.getY()) % 2 == 0) {
-                setLocation(mouse.getX(), mouse.getY());
-            }
+            setLocation(mouse.getX(), mouse.getY());
         }
         
         //gets performed when the user drops the checker on the board
         if(Greenfoot.mouseDragEnded(this)){
+            if(!isValidSpot(startX, startY, getX(), getY())){
+                setLocation(startX, startY);
+            }
             Bomb b = (Bomb)getOneIntersectingObject(Bomb.class);
             if(b != null) {
                 getWorld().removeObject(b);
                 getWorld().removeObject(this);
             }
         }
+    }
+    
+    //Helper method that checks the validity of a position relative to the checker's start position
+    private boolean isValidSpot(int xStart, int yStart, int xMoved, int yMoved) {
+        //iterated through all possible x and y positions for diagonal movement, returns true if
+        for(int i = 0; i < 9; i++) {
+            if(((xMoved == xStart + i) || (xMoved == xStart - i)) &&
+               ((yMoved == yStart + i) || (yMoved == yStart - i))) {
+                   return true;
+                }
+        }
+        return false;
     }
 }
